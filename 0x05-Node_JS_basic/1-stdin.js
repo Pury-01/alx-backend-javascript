2a9522e0c4537f9ed0d182d99b6124353409d48e
Pury-01 <puryokumu@gmail.com>
2024-11-29 18:45:23 +0300

    fixed styling errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,16 +4,16 @@ const readline = require('readline');
 
 // create interface for reading input from sttdin
 const r1 = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-r1.question("Welcome to Holberton School, what is your name?\n", (INPUT) => {
-	console.log(`Your name is: ${INPUT}`);
-	r1.close(); // close interface
+r1.question('Welcome to Holberton School, what is your name?\n', (INPUT) => {
+  console.log(`Your name is: ${INPUT}`);
+  r1.close(); // close interface
 });
 
 r1.on('close', () => {
-	console.log("This important software is now closing");
-	process.exit(0);
+  console.log('This important software is now closing');
+  process.exit(0);
 });
